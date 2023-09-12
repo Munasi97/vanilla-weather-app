10c4a7303893fb2673afdbcb445c851bad648893
Munasi97 <129224515+Munasi97@users.noreply.github.com>
2023-09-12 22:02:37 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,8 +43,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "4bca6172d5f0a5497494fcb53715a62c";
-let city = "Pretoria";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "4bca6172d5f0a5497494fcb53715a62c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+
+search("Pretoria");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
