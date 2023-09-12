f0c3ac10c84da78d4380cabc3e2974e6d9d998a5
Munasi97 <129224515+Munasi97@users.noreply.github.com>
2023-09-12 20:29:41 +0200

    Removed console log

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let tempElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
