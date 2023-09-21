7b9f95cdadee4944e2d8b70cfb0c873b479b1eda
Munasi97 <129224515+Munasi97@users.noreply.github.com>
2023-09-22 01:20:40 +0200

    Called a 7 day forecast API

--- src/app.js
+++ src/app.js
@@ -17,28 +17,22 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
+
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
-  let days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   let forecastHTML = `  <div class="row">`;
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
       `
-    <div class="col-2">
+    <div class="col-sm-2">
       <div class="weather-forecast-date">${day}</div>
       <img
         src="https://openweathermap.org/img/wn/01d@2x.png"
@@ -55,7 +49,13 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -80,6 +80,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -125,4 +127,3 @@ let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("Pretoria");
-displayForecast();
